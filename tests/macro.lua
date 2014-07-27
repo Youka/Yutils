f0ca486b809b5d152e1ce1d8d823762ea9b66646
Youka <spanknebel.borken@t-online.de>
2014-07-27 15:27:36 +0200

    tests updated (matrix use simplified; macro with font list & better config precisions)

--- tests/macro.lua
+++ tests/macro.lua
@@ -1,9 +1,14 @@
+-- Script information
 script_name = "Wobble text"
 script_description = "Converts a text to a shape and adds wobbling."
 script_author = "Youka"
-script_version = "1.1"
-script_modified = "5th July 2014"
+script_version = "1.2"
+script_modified = "27th July 2014"
 
+-- Load Yutils library
+local Yutils = include("Yutils.lua")
+
+-- UI configuration template
 local config_template = {
 	{
 		class = "label",
@@ -11,9 +16,9 @@ local config_template = {
 		label = "Fontname: "
 	},
 	{
-		class = "edit", name = "fontname",
+		class = "dropdown", name = "fontname",
 		x = 1, y = 0, width = 3, height = 1,
-		hint = "Valid font family", text = "Arial"
+		hint = "Font family" -- items = {}, value = ""
 	},
 	{
 		class = "checkbox", name = "bold",
@@ -33,7 +38,7 @@ local config_template = {
 	{
 		class = "intedit", name = "fontsize",
 		x = 1, y = 1, width = 3, height = 1,
-		hint = "Valid font size", value = 20, min = 1, max = 1000
+		hint = "Font size", value = 30, min = 1, max = 1000
 	},
 	{
 		class = "checkbox", name = "underline",
@@ -51,9 +56,9 @@ local config_template = {
 		label = "Scale X%: "
 	},
 	{
-		class = "intedit", name = "scale_x",
+		class = "floatedit", name = "scale_x",
 		x = 1, y = 2, width = 1, height = 1,
-		hint = "Horizontal scaling in percent", value = 100, min = 1, max = 10000
+		hint = "Horizontal scaling in percent", value = 100, min = 0.01, max = 10000, step = 0.01
 	},
 	{
 		class = "label",
@@ -61,9 +66,9 @@ local config_template = {
 		label = "Scale Y%: "
 	},
 	{
-		class = "intedit", name = "scale_y",
+		class = "floatedit", name = "scale_y",
 		x = 3, y = 2, width = 1, height = 1,
-		hint = "Vertical scaling in percent", value = 100, min = 1, max = 10000
+		hint = "Vertical scaling in percent", value = 100, min = 0.01, max = 10000, step = 0.01
 	},
 	{
 		class = "label",
@@ -71,9 +76,9 @@ local config_template = {
 		label = "Spacing: "
 	},
 	{
-		class = "intedit", name = "spacing",
+		class = "floatedit", name = "spacing",
 		x = 5, y = 2, width = 1, height = 1,
-		hint = "Intercharacter spacing", value = 0, min = -100, max = 100
+		hint = "Intercharacter spacing", value = 0, min = -100, max = 100, step = 0.01
 	},
 	{
 		class = "label",
@@ -101,14 +106,14 @@ local config_template = {
 		label = "Wobble frequency: "
 	},
 	{
-		class = "intedit", name = "wobble_frequency_x",
+		class = "floatedit", name = "wobble_frequency_x",
 		x = 1, y = 7, width = 1, height = 1,
-		hint = "Horizontal wobbling frequency in percent", value = 0, min = 0, max = 99
+		hint = "Horizontal wobbling frequency in percent", value = 0, min = 0, max = 10, step = 0.00001
 	},
 	{
-		class = "intedit", name = "wobble_frequency_y",
+		class = "floatedit", name = "wobble_frequency_y",
 		x = 2, y = 7, width = 1, height = 1,
-		hint = "Vertical wobbling frequency in percent", value = 0, min = 0, max = 99
+		hint = "Vertical wobbling frequency in percent", value = 0, min = 0, max = 10, step = 0.00001
 	},
 	{
 		class = "label",
@@ -116,21 +121,35 @@ local config_template = {
 		label = "Wobble strength: "
 	},
 	{
-		class = "intedit", name = "wobble_strength_x",
+		class = "floatedit", name = "wobble_strength_x",
 		x = 1, y = 8, width = 1, height = 1,
-		hint = "Horizontal wobbling strength in pixels", value = 0, min = 0, max = 100
+		hint = "Horizontal wobbling strength in pixels", value = 0, min = 0, max = 100, step = 0.01
 	},
 	{
-		class = "intedit", name = "wobble_strength_y",
+		class = "floatedit", name = "wobble_strength_y",
 		x = 2, y = 8, width = 1, height = 1,
-		hint = "Vertical wobbling strength in pixels", value = 0, min = 0, max = 100
+		hint = "Vertical wobbling strength in pixels", value = 0, min = 0, max = 100, step = 0.01
 	},
 }
+do
+	-- Fill font families in configuration
+	local items, items_n = {}, 0
+	for _, family in ipairs(Yutils.decode.list_fonts()) do
+		items_n = items_n + 1
+		items[items_n] = family.name
+	end
+	config_template[2].items = items
+	config_template[2].value = items[1]
+end
 
+-- Macro execution
 local function load_macro()
+	-- Show UI
 	local ok, config = aegisub.dialog.display(config_template, {"Calculate"})
+	-- OK from UI
 	if ok then
-		config_template[2].text = config.fontname
+		-- Save UI configuration to template
+		config_template[2].value = config.fontname
 		config_template[3].value = config.bold
 		config_template[4].value = config.italic
 		config_template[6].value = config.fontsize
@@ -144,7 +163,7 @@ local function load_macro()
 		config_template[21].value = config.wobble_frequency_y
 		config_template[23].value = config.wobble_strength_x
 		config_template[24].value = config.wobble_strength_y
-		local Yutils = include("Yutils.lua")
+		-- Calculate shape from configuration settings
 		local text_shape = Yutils.decode.create_font(config.fontname, config.bold, config.italic, config.underline, config.strikeout, config.fontsize, config.scale_x / 100, config.scale_y / 100, config.spacing).text_to_shape(config.text)
 		if (config.wobble_frequency_x > 0 and config.wobble_strength_x > 0) or (config.wobble_frequency_y > 0 and config.wobble_strength_y > 0) then
 			text_shape = Yutils.shape.filter(
@@ -155,13 +174,15 @@ local function load_macro()
 										1.5
 									),
 									function(x,y)
-										return x + math.sin(y * math.pi * 2 * 0.01 * config.wobble_frequency_x) * config.wobble_strength_x,
-												y + math.sin(x * math.pi * 2 * 0.01 * config.wobble_frequency_y) * config.wobble_strength_y
+										return x + math.sin(y * config.wobble_frequency_x * math.pi * 2) * config.wobble_strength_x,
+												y + math.sin(x * config.wobble_frequency_y * math.pi * 2) * config.wobble_strength_y
 									end
 								)
 		end
+		-- Show calculated shape
 		aegisub.log(text_shape)
 	end
 end
 
+-- Register macro to Aegisub
 aegisub.register_macro(script_name,script_description,load_macro)
